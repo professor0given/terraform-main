813e86ca5f4e56845252a633d7e6257d5268ae00
Rohit <118429797+professor0given@users.noreply.github.com>
2026-05-27 00:28:30 +0530

    Update rg.tf

--- rg.tf
+++ rg.tf
@@ -2,3 +2,8 @@ resource "azurerm_resource_group" "rg-1" {
     name = "rg-india"
     location = "Central India"
 }
+
+resource "azurerm_resource_group" "rg-2" {
+    name = "rg-2"
+    location = "Central India"
+}
